1a22c3dd6864cdbbee3e0db88e4e024f975421b7
Martin Straus <martinstraus@gmail.com>
2024-11-03 12:29:51 -0300

    Scanner and compiler scaffolding.

--- vm.h
+++ vm.h
@@ -20,9 +20,9 @@ typedef enum {
 } InterpretResult;
 
 void initVM();
-InterpretResult interpret(Chunk* chunk);
+void freeVM();
+InterpretResult interpret(const char* source);
 void push(Value value);
 Value pop();
-void freeVM();
 
 #endif
