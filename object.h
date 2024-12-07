c9e614414632706b433725ba6f3099dc68ee75c9
Martin Straus <martinstraus@gmail.com>
2024-12-07 14:26:34 -0300

    Added AS_STRING and AS_CSTRING macros.

--- object.h
+++ object.h
@@ -6,6 +6,9 @@
 #include "value.h"
 
 #define OBJ_TYPE(value) (AS_OBJ(value)->type)
+#define IS_STRING(value) isObjType(value, OBJ_STRING)
+#define AS_STRING(value) ((ObjString*)AS_OBJ(value))
+#define AS_CSTRING(value) (((ObjString*)AS_OBJ(value))->chars)
 
 typedef enum {
     OBJ_STRING,
@@ -19,6 +22,10 @@ struct ObjString {
     Obj obj;
     int length;
     char* chars;
+};
+
+static inline bool isObjType(Value value, ObjType type) {
+    return IS_OBJ(value) && AS_OBJ(value)->type == type;
 }
 
 #endif
