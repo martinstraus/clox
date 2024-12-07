e3e0192c315babe07365c7a60be31af03edbf1f1
Martin Straus <martinstraus@gmail.com>
2024-12-07 14:39:14 -0300

    Added macros and functions for parsing strings.

--- memory.h
+++ memory.h
@@ -1,6 +1,10 @@
 #ifndef chunk_memory_h
 #define chunk_memory_h
 
+#include "common.h"
+
+#define ALLOCATE(type, count) (type*)reallocate(NULL, 0, sizeof(type) * (count))
+
 #define GROW_CAPACITY(capacity) ((capacity) < 8 ? 8 : (capacity) * 2)
 
 #define FREE_ARRAY(type, pointer, oldCount) reallocate(pointer, sizeof(type) * (oldCount), 0)
