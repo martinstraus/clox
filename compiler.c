15844ae144b34427b9134d35aed35444445b7206
Martin Straus <martinstraus@gmail.com>
2024-12-21 12:55:12 -0300

    Added expression statements.

--- compiler.c
+++ compiler.c
@@ -255,6 +255,12 @@ static void expression() {
     parsePrecedence(PREC_ASSIGNMENT);
 }
 
+static void expressionStatement() {
+    expression();
+    consume(TOKEN_SEMICOLON, "Expect ';' after expression");
+    emitByte(OP_POP);
+}
+
 static void printStatement() {
     expression();
     consume(TOKEN_SEMICOLON, "Expect ';' after value.");
@@ -268,6 +274,8 @@ static void declaration() {
 static void statement() {
     if (match(TOKEN_PRINT)) {
         printStatement();
+    } else {
+        expressionStatement();
     }
 }
 
