e8f04576a734931a195be5483c9cb4969c08b8a4
Martin Straus <martinstraus@gmail.com>
2025-02-02 13:03:33 -0300

    Class definitions.

--- memory.c
+++ memory.c
@@ -64,6 +64,11 @@ static void blackenObject(Obj* object) {
     printf("\n");
 #endif
     switch (object->type) {
+        case OBJ_CLASS: {
+            ObjClass* klass = (ObjClass*)object;
+            markObject((Obj*)klass->name);
+            break;
+        }
         case OBJ_CLOSURE: {
             ObjClosure* closure = (ObjClosure*)object;
             markObject((Obj*)closure->function);
@@ -114,6 +119,10 @@ void freeObject(Obj* object) {
     printf("%p free type %d\n", (void*)object, object->type);
 #endif
     switch(object->type) {
+        case OBJ_CLASS: {
+            FREE(ObjClass, object),
+            break;
+        }
         case OBJ_CLOSURE: {
             ObjClosure* closure = (ObjClosure*) object;
             FREE_ARRAY(ObjUpvalue*, closure->upvalues, closure->upvalueCount);
--- object.c
+++ object.c
@@ -30,6 +30,12 @@ ObjFunction* newFunction() {
     return function;
 }
 
+ObjClass* newClass(ObjString* name) {
+    ObjClass* klass = ALLOCATE_OBJ(ObjClass, OBJ_CLASS);
+    klass->name = name;
+    return klass;
+}
+
 ObjClosure* newClosure(ObjFunction* function) {
     ObjClosure* closure = ALLOCATE_OBJ(ObjClosure, OBJ_CLOSURE);
     ObjUpvalue** upvalues = ALLOCATE(ObjUpvalue*, function->upvalueCount);
@@ -106,6 +112,9 @@ static void printFunction(ObjFunction* function) {
 
 void printObject(Value value) {
     switch(OBJ_TYPE(value)) {
+        case OBJ_CLASS:
+            printf("%s", AS_CLASS(value)->name->chars);
+            break;
         case OBJ_CLOSURE:
             printFunction(AS_CLOSURE(value)->function);
             break;
